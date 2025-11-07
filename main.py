4865473b2c2b9b1d0c1686a0a649cac441673c84
Saksham <sakshamdhull442@gmail.com>
2025-11-07 15:01:09 +0530

    ollama

--- main.py
+++ main.py
@@ -1,37 +1,50 @@
 import os
 
 import streamlit as st
-from dotenv import load_dotenv
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.outputs import ChatGeneration
-from langchain_core.prompts import PromptTemplate
-from langchain_core.prompts.string import PromptTemplateFormat
-from langchain_openai import ChatOpenAI
 
-load_dotenv()
+# from dotenv import load_dotenv
+# from langchain_core.output_parsers import StrOutputParser
+# from langchain_core.outputs import ChatGeneration
+# from langchain_core.prompts import PromptTemplate
+# from langchain_core.prompts.string import PromptTemplateFormat
+# from langchain_openai import ChatOpenAI
+from ollama import ChatResponse, Client, chat
+
+# load_dotenv()
 
 
-if not os.environ.get("OPENROUTER_API_KEY"):
-    raise ValueError("API_KEY environment variable is not set")
+# if not os.environ.get("OPENROUTER_API_KEY"):
+#     raise ValueError("API_KEY environment variable is not set")
+
+MODEL = "gemma3:1b"
+MAXTOKENS = 100
+TEMPERATURE = 0.3
 
 
 class ContextBuilder:
-    def __init__(self, llm):
-        self.llm = llm
-        self.template = "summarize this conversation include both user and bot roles,conversation: {latesthistory}"
-        self.input_variables = ["latesthistory"]
+    def __init__(self, llm_client):
+        self.llm_client = llm_client
+        self.template = {
+            "role": "system",
+            "content": f"Build a short term memory using the conversation in {MAXTOKENS} tokens, mention all specific things talked about. output must include all names and details that were mentioned. It should be alpha numeric and punctuation only, no special formatting, keep it as concise as possible, should not be human readable. Keep under 100 words, Output format: Query: "
+            "{topic} and answer: {answer}"
+            "",
+        }
         self.context = ""
-        self.llm_prompt = PromptTemplate(
-            template=self.template, input_variables=self.input_variables
-        )
-        self.llm_chain = self.llm_prompt | self.llm | StrOutputParser()
 
     def invoke(self, input):
         """
         Only input the latesthistory
         """
 
-        self.context += "next " + self.llm_chain.invoke({"latesthistory": input})
+        self.context += (
+            "next "
+            + self.llm_client.chat(
+                model=MODEL,
+                messages=[self.template, {"role": "user", "content": input}],
+                options={"num_predict": MAXTOKENS, "temperature": TEMPERATURE},
+            ).message.content
+        )
         print(f"CONTEXT: {self.context}")
 
     def get(self):
@@ -39,45 +52,43 @@ class ContextBuilder:
 
 
 class ChatGenerator:
-    def __init__(self, llm):
-        self.llm = llm
-        self.context = ContextBuilder(llm)
-        self.template = (
-            "This is the past converstaion {history}, Give a reponse to {query}."
-        )
-        self.input_variables = ["history", "query"]
-        self.llm_prompt = PromptTemplate(
-            template=self.template, input_variables=self.input_variables
-        )
-        self.llm_chain = self.llm_prompt | self.llm | StrOutputParser()
+    def __init__(self, llm_client):
+        self.llm_client = llm_client
+        self.context = ContextBuilder(llm_client)
+        self.template = {
+            "role": "system",
+            "content": f"Give a response in {MAXTOKENS} tokens to user query given this memory: ",
+        }
 
     def invoke(self, input):
-        output = self.llm_chain.invoke({"query": input, "history": self.context.get()})
+        template = self.template
+        template["content"] += self.context.get()
+        output = self.llm_client.chat(
+            model=MODEL,
+            messages=[template, {"role": "user", "content": input}],
+            options={"num_predict": MAXTOKENS, "temperature": TEMPERATURE},
+        ).message.content
         self.context.invoke("User Asked:" + input + " and Bot replied:" + output)
         return output
 
 
 st.title("Contextual Chatty bot")
+if st.button("reload"):
+    # Reset the context builder
+    st.session_state.context_builder = ContextBuilder(st.session_state.llm_client)
+    st.session_state.messages = []
+    st.rerun()
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.write(message["content"])
-if "llm" not in st.session_state:
-    st.session_state.llm = ChatOpenAI(
-        api_key=os.environ.get("OPENROUTER_API_KEY"),
-        base_url="https://openrouter.ai/api/v1",
-        max_tokens=100,
-        temperature=0.7,
-    )
+if "llm_client" not in st.session_state:
+    st.session_state.llm_client = Client(host="http://localhost:11434")
 if "chatgenerator" not in st.session_state:
-    st.session_state.chatgenerator = ChatGenerator(st.session_state.llm)
-if st.button("reload"):
-    # Reset the context builder
-    st.session_state.context_builder = ContextBuilder(st.session_state.llm)
-    st.session_state.messages = []
-    st.rerun()
+    st.session_state.chatgenerator = ChatGenerator(st.session_state.llm_client)
+
 chatgenerator = st.session_state.chatgenerator
 
 if userinput := st.chat_input("Have a question boi?"):
---
+++ ollama.sh
@@ -0,0 +1,3 @@
+MODEL="gemma3:1b"
+brew services start ollama
+# ollama run gemma3:1b &
